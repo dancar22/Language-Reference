5d065ef4c9a37be4b4b71bb40409fe90d6b793c9
Dan Caraman <dcaraman@berkeley.edu>
2023-09-01 18:43:12 -0700

    develop initial code for bot with basic support for C++

---
+++ main.py
@@ -0,0 +1,95 @@
+import discord 
+import certifi
+from urllib.request import urlopen
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+current_url = "https://cplusplus.com/reference/"
+current_html = ""
+html_bytes = ""
+current_level = 0
+
+def getMessage(msg):
+    start_index, end_index = msg.find(">") + 1, msg[1:].find("<") 
+    return msg[start_index: end_index]
+  
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+     
+@client.event
+async def on_message(message):
+    global current_level
+    global current_url
+    
+    if message.author == client.user:
+        return
+    
+    if message.content == 'home' or message.content == 'Home':
+        current_level = 0
+        current_url = "https://cplusplus.com/reference/"
+    
+    page = urlopen(current_url, cafile=certifi.where())
+    html_bytes = page.read()
+    current_html = html_bytes.decode("utf-8")
+    
+    if current_level == 0:
+        
+        output = "Here is a list of topics:\n"
+        
+        while "dd" in current_html:
+            
+            if len(output) > 1950:
+                await message.channel.send(output)
+                output = ""
+                
+            start_index, end_index = current_html.find("<dd>"), current_html.find("</dd>")
+            output += ("\t" + getMessage(current_html[start_index:end_index]) + "\n")
+            current_html = current_html[(end_index + 5):]
+            
+        await message.channel.send(output)
+        current_level = 1
+        await message.channel.send("Type one of the options to learn more")
+        
+    elif current_level == 1:
+        
+        index = current_html.find(message.content)
+        if index < 90:
+            await message.channel.send("I did not find the option you picked, try again...")
+        else:
+            finding_url = current_html[index - 90: index]
+            starting_index = finding_url.find("href")
+            finding_url = "https://cplusplus.com" + finding_url[starting_index + 7:]
+            end_index = finding_url.find("\">")
+            finding_url = finding_url[:end_index]
+            current_url = finding_url
+            await message.channel.send(finding_url)
+            
+            page = urlopen(current_url, cafile=certifi.where())
+            html_bytes = page.read()
+            current_html = html_bytes.decode("utf-8")
+            
+            output = "Here is a list of topics:\n"
+            
+        
+            while "dd" in current_html:
+            
+                if len(output) > 1950:
+                    await message.channel.send(output)
+                    output = ""
+                
+                start_index, end_index = current_html.find("<dd>"), current_html.find("</dd>")
+                output += ("\t" + getMessage(current_html[start_index:end_index]) + "\n")
+                current_html = current_html[(end_index + 5):]
+            
+            await message.channel.send(output)
+            current_level = 1
+            await message.channel.send("Type one of the options to learn more")
+            
+            
+
+
+
+client.run('MTE0NzI2MDQ3NTkxOTMxNTEyNA.GorPAZ.O24kFg3Vf4gDIX7zYDTXT7CIcUI2X1su9hzevE')    
