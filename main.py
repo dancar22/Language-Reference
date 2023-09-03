5c5bd0211caddd70b09571f25bfc2f896455c9ff
Dan Caraman <dcaraman@berkeley.edu>
2023-09-03 11:48:02 -0700

    add  command support for C++

--- main.py
+++ main.py
@@ -33,62 +33,41 @@ async def on_ready():
      
 @client.event
 async def on_message(message):
+
     global current_level
     global current_url
+    global current_code
+
+
     
-    if message.author == client.user:
+    if message.author == client.user or not message.content.startswith("$"):
         return
-    
+    print(current_code)
+
     if message.content == "$help":
         await help(message)
         return
+
     
-    if message.content == '$home' or message.content == '$Home':
+    if message.content == "$cpp":
+        current_code = 1
         current_level = 0
         current_url = "https://cplusplus.com/reference/"
+        await message.channel.send("Now working with https://cplusplus.com/reference/")
+        
+
+    if message.content == '$home' or message.content == '$Home':
+        current_level = 0
+        current_code = 0
+        
+    if current_code == 1:
+        page = urlopen(current_url, cafile=certifi.where())
+        html_bytes = page.read()
+        current_html = html_bytes.decode("utf-8")
     
-    page = urlopen(current_url, cafile=certifi.where())
-    html_bytes = page.read()
-    current_html = html_bytes.decode("utf-8")
-    
-    if current_level == 0:
+        if current_level == 0:
         
-        output = "Here is a list of topics:\n"
-        
-        while "dd" in current_html:
-            
-            if len(output) > 1950:
-                await message.channel.send(output)
-                output = ""
-                
-            start_index, end_index = current_html.find("<dd>"), current_html.find("</dd>")
-            output += ("\t" + getMessage(current_html[start_index:end_index]) + "\n")
-            current_html = current_html[(end_index + 5):]
-            
-        await message.channel.send(output)
-        current_level = 1
-        await message.channel.send("Type one of the options to learn more")
-        
-    elif current_level == 1:
-        
-        index = current_html.find(message.content)
-        if index < 90:
-            await message.channel.send("I did not find the option you picked, try again...")
-        else:
-            finding_url = current_html[index - 90: index]
-            starting_index = finding_url.find("href")
-            finding_url = "https://cplusplus.com" + finding_url[starting_index + 7:]
-            end_index = finding_url.find("\">")
-            finding_url = finding_url[:end_index]
-            current_url = finding_url
-            await message.channel.send(finding_url)
-            
-            page = urlopen(current_url, cafile=certifi.where())
-            html_bytes = page.read()
-            current_html = html_bytes.decode("utf-8")
-            
             output = "Here is a list of topics:\n"
-            
         
             while "dd" in current_html:
             
@@ -103,6 +82,41 @@ async def on_message(message):
             await message.channel.send(output)
             current_level = 1
             await message.channel.send("Type one of the options to learn more")
+        
+        elif current_level == 1:
+        
+            index = current_html.find(message.content[4:])
+            if index < 90:
+                await message.channel.send("I did not find the option you picked, try again...")
+            else:
+                finding_url = current_html[index - 90: index]
+                starting_index = finding_url.find("href")
+                finding_url = "https://cplusplus.com" + finding_url[starting_index + 7:]
+                end_index = finding_url.find("\">")
+                finding_url = finding_url[:end_index]
+                current_url = finding_url
+                await message.channel.send(finding_url)
+            
+                page = urlopen(current_url, cafile=certifi.where())
+                html_bytes = page.read()
+                current_html = html_bytes.decode("utf-8")
+            
+                output = "Here is a list of topics:\n"
+            
+        
+                while "dd" in current_html:
+            
+                    if len(output) > 1950:
+                        await message.channel.send(output)
+                        output = ""
+                
+                    start_index, end_index = current_html.find("<dd>"), current_html.find("</dd>")
+                    output += ("\t" + getMessage(current_html[start_index:end_index]) + "\n")
+                    current_html = current_html[(end_index + 5):]
+            
+                await message.channel.send(output)
+                current_level = 1
+                await message.channel.send("Type one of the options to learn more")
             
             
 
